06f6c42a341cfacd9310fd515d74a9312a5cf4de
Luan Otoni <luanotoni4@gmail.com>
2024-04-16 11:08:46 -0300

    Commit atualizações até o passo 7.

--- app/src/main/java/otoni/luan/lista/activity/MainActivity.java
+++ app/src/main/java/otoni/luan/lista/activity/MainActivity.java
@@ -1,6 +1,8 @@
-package otoni.luan.lista;
+package otoni.luan.lista.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import otoni.luan.lista.R;
+
+public class MainActivity extends AppCompatActivity {
+    static int NEW_ITEM_REQUEST =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+        FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NewItemActivity.class);
+                startActivityForResult(i,NEW_ITEM_REQUEST);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/otoni/luan/lista/activity/NewItemActivity.java
@@ -0,0 +1,36 @@
+package otoni.luan.lista.activity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import otoni.luan.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
+
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_item);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageButton imgCI = findViewById(R.id.imbCI);
+        imgCI.setOnClickListener();
+
+    }
+}
